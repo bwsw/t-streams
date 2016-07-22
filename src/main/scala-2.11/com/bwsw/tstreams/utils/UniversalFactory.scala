9549cbcccffe40b0464bbdfd03db83210d69baf9
Bitworks, Ltd <bitworks@bw-sw.com>
2016-07-22 15:23:18 +0700

    Started UniversalFactory producer-related methods.

--- src/main/scala-2.11/com/bwsw/tstreams/utils/UniversalFactory.scala
+++ src/main/scala-2.11/com/bwsw/tstreams/utils/UniversalFactory.scala
@@ -1,18 +1,28 @@
 package com.bwsw.tstreams.utils
 
+import java.net.{InetSocketAddress}
+import java.security.InvalidParameterException
+
+import com.aerospike.client.Host
+import com.aerospike.client.policy.{Policy, WritePolicy, ClientPolicy}
 import com.bwsw.tstreams.agents.consumer.BasicConsumer
 import com.bwsw.tstreams.agents.consumer.subscriber.{BasicSubscriberCallback, BasicSubscribingConsumer}
 import com.bwsw.tstreams.agents.producer.BasicProducer
 import com.bwsw.tstreams.converter.IConverter
+import com.bwsw.tstreams.data.IStorage
+import com.bwsw.tstreams.data.aerospike.{AerospikeStorageOptions, AerospikeStorageFactory}
+import com.bwsw.tstreams.data.cassandra.{CassandraStorageOptions, CassandraStorageFactory}
 import com.bwsw.tstreams.generator.IUUIDGenerator
+import com.bwsw.tstreams.metadata.MetadataStorageFactory
 import com.datastax.driver.core.policies.RoundRobinPolicy
+import org.omg.PortableServer.THREAD_POLICY_ID
 
 import scala.collection.mutable.{Map, HashMap}
 
 /**
   * Class which holds definitions for UniversalFactory
   */
-public object UF_Dictionary {
+object UF_Dictionary {
 
   /**
     * UF_Dictionary actor-system scope
@@ -90,6 +100,25 @@ public object UF_Dictionary {
           * Definition for cassandra backend
           */
         val DATA_DRIVER_CASSANDRA = "cassandra"
+      }
+
+      /**
+        * scope UF_Dictionary.Data.Cluster aerospike engine
+        */
+      object Aerospike {
+        /**
+          * user can specify write policy if required
+          */
+        val write_policy = "data.cluster.aerospike.write-policy"
+        /**
+          * user can specify read policy if required
+          */
+        val read_policy = "data.cluster.aerospike.read-policy"
+        /**
+          * user can specify client policy
+          */
+        val client_policy = "data.cluster.aerospike.client-policy"
+
       }
     }
   }
@@ -184,7 +213,19 @@ public object UF_Dictionary {
         */
       val distribution_policy = "producer.transaction.distribution-policy" // TODO: fix internals write->distribution
 
+      /**
+        * UF_Dictionary.Producer.Transaction consts scope
+        */
+      object Consts {
+        /**
+          * defines standard round-robin policy
+          */
+        val DISTRIBUTION_POLICY_RR = "round-robin"
+      }
+
     }
+
+
   }
 
   /**
@@ -229,27 +270,38 @@ class UniversalFactory {
 
   var propertyMap = new HashMap[String,Any]()
 
+  // common
   propertyMap += (UF_Dictionary.ActorSystem.name            -> "T-Streams")
+
+  // metadata cluster scope
   propertyMap += (UF_Dictionary.Metadata.Cluster.endpoints  -> "localhost:9042")
   propertyMap += (UF_Dictionary.Metadata.Cluster.login      -> null)
   propertyMap += (UF_Dictionary.Metadata.Cluster.password   -> null)
 
+  // data cluster scope
   propertyMap += (UF_Dictionary.Data.Cluster.driver         -> UF_Dictionary.Data.Cluster.Consts.DATA_DRIVER_AEROSPIKE)
   propertyMap += (UF_Dictionary.Data.Cluster.endpoints      -> "localhost:3000")
   propertyMap += (UF_Dictionary.Data.Cluster.namespace      -> "test")
   propertyMap += (UF_Dictionary.Data.Cluster.login          -> null)
   propertyMap += (UF_Dictionary.Data.Cluster.password       -> null)
+  propertyMap += (UF_Dictionary.Data.Cluster.Aerospike.write_policy      -> null)
+  propertyMap += (UF_Dictionary.Data.Cluster.Aerospike.read_policy       -> null)
+  propertyMap += (UF_Dictionary.Data.Cluster.Aerospike.client_policy     -> null)
 
+
+  // coordination scope
   propertyMap += (UF_Dictionary.Coordination.endpoints          -> "localhost:2181")
   propertyMap += (UF_Dictionary.Coordination.root               -> "/t-streams")
   propertyMap += (UF_Dictionary.Coordination.ttl                -> 7)
   propertyMap += (UF_Dictionary.Coordination.connection_timeout -> 7)
 
+  // stream scope
   propertyMap += (UF_Dictionary.Stream.name                 -> "test")
   propertyMap += (UF_Dictionary.Stream.partitions           -> 1)
   propertyMap += (UF_Dictionary.Stream.ttl                  -> 60 * 60 * 24)
   propertyMap += (UF_Dictionary.Stream.description          -> "Test stream")
 
+  // producer scope
   propertyMap += (UF_Dictionary.Producer.master_bind_host                     -> "localhost")
   propertyMap += (UF_Dictionary.Producer.master_bind_port                     -> 18000)
   propertyMap += (UF_Dictionary.Producer.master_timeout                       -> 5)
@@ -257,16 +309,18 @@ class UniversalFactory {
   propertyMap += (UF_Dictionary.Producer.Transaction.open_maxwait             -> 5)
   propertyMap += (UF_Dictionary.Producer.Transaction.keep_alive               -> 2)
   propertyMap += (UF_Dictionary.Producer.Transaction.data_write_batch_size    -> 100)
-  propertyMap += (UF_Dictionary.Producer.Transaction.distribution_policy      -> RoundRobinPolicy)
+  propertyMap += (UF_Dictionary.Producer.Transaction.distribution_policy      -> UF_Dictionary.Producer.Transaction.Consts.DISTRIBUTION_POLICY_RR)
   propertyMap += (UF_Dictionary.Producer.thread_pool                          -> 4)
 
-
-  propertyMap += (UF_Dictionary.Consumer.transaction_preload      -> 10)
-  propertyMap += (UF_Dictionary.Consumer.data_preload             -> 100)
-
+  // consumer scope
+  propertyMap += (UF_Dictionary.Consumer.transaction_preload              -> 10)
+  propertyMap += (UF_Dictionary.Consumer.data_preload                     -> 100)
   propertyMap += (UF_Dictionary.Consumer.Subscriber.bind_host             -> "localhost")
   propertyMap += (UF_Dictionary.Consumer.Subscriber.bind_port             -> 18001)
   propertyMap += (UF_Dictionary.Consumer.Subscriber.persistent_queue_path -> "/tmp")
+
+  //metadata/data factories
+  val msFactory = new MetadataStorageFactory
 
   /**
     *
@@ -289,21 +343,98 @@ class UniversalFactory {
     */
   def getProperty(key: String): Any = propertyMap get key
 
+  def getAerospikeCompatibleHostList(h: String): List[Host] =
+    h.split(',').map((sh: String) => new Host(sh.split(':').head, Integer.parseInt(sh.split(':').tail.head))).toList
+
+  def getCassandraCompatibleHostList(h: String): List[InetSocketAddress] =
+    h.split(',').map((sh: String) => new InetSocketAddress(sh.split(':').head, Integer.parseInt(sh.split(':').tail.head))).toList
+
   /**
     *
     * @param isLowPriority
     * @param txnGenerator
     * @param converter
     * @param partitions
-    * @tparam F
-    * @tparam T
+    * @tparam USERTYPE - type convert data from
     * @return
     */
-  def getProducer[F,T](isLowPriority : Boolean,
-                       txnGenerator : IUUIDGenerator,
-                       converter : IConverter[F,T],
-                       partitions : List
-                      ): BasicProducer = {
+  def getProducer[USERTYPE](isLowPriority : Boolean,
+                       txnGenerator  : IUUIDGenerator,
+                       converter     : IConverter[USERTYPE,Array[Byte]],
+                       partitions    : List[Int]
+                      ): BasicProducer[USERTYPE,Array[Byte]] = {
+
+    var ds: IStorage[Array[Byte]] = null
+
+    if ((propertyMap get UF_Dictionary.Data.Cluster.driver).equals(UF_Dictionary.Data.Cluster.Consts.DATA_DRIVER_AEROSPIKE))
+    {
+      val dsf = new AerospikeStorageFactory
+
+      // construct client policy
+      var cp: ClientPolicy = null
+      if (propertyMap.get(UF_Dictionary.Data.Cluster.Aerospike.client_policy) == null)
+        cp = new ClientPolicy()
+      else
+        cp = propertyMap.get(UF_Dictionary.Data.Cluster.Aerospike.client_policy).asInstanceOf[ClientPolicy]
+
+      val dsLogin = propertyMap.get(UF_Dictionary.Data.Cluster.login)
+      if (dsLogin != null)
+        cp.user = dsLogin.toString
+
+      val dsPassword = propertyMap.get(UF_Dictionary.Data.Cluster.password)
+      if (dsPassword != null)
+        cp.password = dsPassword.toString
+
+      // construct write policy
+      var wp: WritePolicy = null
+      if (propertyMap.get(UF_Dictionary.Data.Cluster.Aerospike.write_policy) == null)
+        wp = new WritePolicy()
+      else
+        wp = propertyMap.get(UF_Dictionary.Data.Cluster.Aerospike.write_policy).asInstanceOf[WritePolicy]
+
+      // construct write policy
+      var rp: Policy = null
+      if (propertyMap.get(UF_Dictionary.Data.Cluster.Aerospike.read_policy) == null)
+        rp = new WritePolicy()
+      else
+        rp = propertyMap.get(UF_Dictionary.Data.Cluster.Aerospike.read_policy).asInstanceOf[Policy]
+
+      val opts = new AerospikeStorageOptions(
+        namespace     = propertyMap.get(UF_Dictionary.Data.Cluster.namespace).toString,
+        hosts         = getAerospikeCompatibleHostList(propertyMap.get(UF_Dictionary.Data.Cluster.endpoints).toString),
+        clientPolicy  = cp,
+        writePolicy   = wp,
+        readPolicy    = rp)
+
+      // create instance of aerospike data storage
+      ds = dsf.getInstance(opts)
+
+    }
+    else if ((propertyMap get UF_Dictionary.Data.Cluster.driver).equals(UF_Dictionary.Data.Cluster.Consts.DATA_DRIVER_CASSANDRA))
+    {
+      val dsf = new CassandraStorageFactory
+
+      val dsLogin = propertyMap.get(UF_Dictionary.Data.Cluster.login)
+      val login = if (dsLogin != null) dsLogin.toString else null
+
+      val dsPassword = propertyMap.get(UF_Dictionary.Data.Cluster.password)
+      val password = if (dsPassword != null) dsPassword.toString else null
+
+      val opts = new CassandraStorageOptions(
+        keyspace = propertyMap.get(UF_Dictionary.Data.Cluster.namespace).toString,
+        cassandraHosts = getCassandraCompatibleHostList(propertyMap.get(UF_Dictionary.Data.Cluster.endpoints).toString),
+        login = login,
+        password = password
+      )
+
+      ds = dsf.getInstance(opts)
+    } else
+    {
+      throw new InvalidParameterException("Only UF_Dictionary.Data.Cluster.Consts.DATA_DRIVER_CASSANDRA and " +
+                                          "UF_Dictionary.Data.Cluster.Consts.DATA_DRIVER_AEROSPIKE engines " +
+                                          "are supported currently in UniversalFactory.")
+    }
+
     null
   }
 
@@ -312,14 +443,13 @@ class UniversalFactory {
     * @param txnGenerator
     * @param converter
     * @param partitions
-    * @tparam F
-    * @tparam T
+    * @tparam USERTYPE type to convert data to
     * @return
     */
-  def getConsumer[F,T](txnGenerator : IUUIDGenerator,
-                       converter : IConverter[F,T],
-                       partitions : List
-                      ): BasicConsumer = {
+  def getConsumer[USERTYPE](txnGenerator : IUUIDGenerator,
+                       converter : IConverter[Array[Byte],USERTYPE],
+                       partitions : List[Int]
+                      ): BasicConsumer[Array[Byte],USERTYPE] = {
     null
   }
 
@@ -329,14 +459,13 @@ class UniversalFactory {
     * @param converter
     * @param partitions
     * @param callback
-    * @tparam F
-    * @tparam T
+    * @tparam USERTYPE - type to convert data to
     * @return
     */
-  def getSubscriber[F,T](txnGenerator : IUUIDGenerator,
-                         converter : IConverter[F,T],
-                         partitions : List,
-                         callback: BasicSubscriberCallback): BasicSubscribingConsumer = {
+  def getSubscriber[USERTYPE](txnGenerator : IUUIDGenerator,
+                         converter : IConverter[Array[Byte],USERTYPE],
+                         partitions : List[Int],
+                         callback: BasicSubscriberCallback[Array[Byte],USERTYPE]): BasicSubscribingConsumer[Array[Byte],USERTYPE] = {
     null
   }
 
