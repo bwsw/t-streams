dd834f860407085ca084dd388c7fb596b9d26566
bwsw <ivan.a.kudryavtsev@gmail.com>
2016-07-23 21:29:18 +0700

    added required slf4j backend jar to deps.

--- src/test/scala-2.11/utils/TStreamsFactoryTest.scala
+++ src/test/scala-2.11/utils/TStreamsFactoryTest.scala
@@ -12,26 +12,27 @@ import testutils._
 
 
 class TStreamsFactoryTest extends FlatSpec with Matchers with BeforeAndAfterAll with TestUtils {
+
+  val randomKeyspace = randomString
+  val cluster = Cluster.builder().addContactPoint("localhost").build()
+  val session = cluster.connect()
+  CassandraHelper.createKeyspace(session, randomKeyspace)
+  CassandraHelper.createMetadataTables(session, randomKeyspace)
+  session.close()
+  cluster.close()
+
+  val f = new TStreamsFactory()
+  f.setProperty(TSF_Dictionary.Metadata.Cluster.namespace,randomKeyspace).
+    setProperty(TSF_Dictionary.Data.Cluster.namespace,"test").
+    setProperty(TSF_Dictionary.Stream.name, "test-stream")
+
+
+  val p = f.getProducer[String](
+    isLowPriority = false,
+    txnGenerator = LocalGeneratorCreator.getGen(),
+    converter = new StringToArrayByteConverter, partitions = List(0) )
+
   "UniversalFactory.getProducer" should "return producer object" in {
-
-    val randomKeyspace = randomString
-    val cluster = Cluster.builder().addContactPoint("localhost").build()
-    val session = cluster.connect()
-    CassandraHelper.createKeyspace(session, randomKeyspace)
-    CassandraHelper.createMetadataTables(session, randomKeyspace)
-    session.close()
-    cluster.close()
-
-    val f = new TStreamsFactory()
-    f.setProperty(TSF_Dictionary.Metadata.Cluster.namespace,randomKeyspace).
-      setProperty(TSF_Dictionary.Data.Cluster.namespace,"test").
-      setProperty(TSF_Dictionary.Stream.name, "test-stream")
-
-
-    val p = f.getProducer[String](
-        isLowPriority = false,
-        txnGenerator = LocalGeneratorCreator.getGen(),
-        converter = new StringToArrayByteConverter, partitions = List(0) )
     p != null shouldEqual true
-   }
+  }
 }
