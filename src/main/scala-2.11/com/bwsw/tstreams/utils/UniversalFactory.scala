df09fffafa84f05b84ac93d2633b161d402faf03
bwsw <ivan.a.kudryavtsev@gmail.com>
2016-07-23 12:11:23 +0700

    Updated universal factory with default/min/max constraints and assertions.

--- src/main/scala-2.11/com/bwsw/tstreams/utils/UniversalFactory.scala
+++ src/main/scala-2.11/com/bwsw/tstreams/utils/UniversalFactory.scala
@@ -8,7 +8,7 @@ import com.aerospike.client.Host
 import com.aerospike.client.policy.{ClientPolicy, Policy, WritePolicy}
 import com.bwsw.tstreams.agents.consumer.BasicConsumer
 import com.bwsw.tstreams.agents.consumer.subscriber.{BasicSubscriberCallback, BasicSubscribingConsumer}
-import com.bwsw.tstreams.agents.producer.InsertionType.SingleElementInsert
+import com.bwsw.tstreams.agents.producer.InsertionType.{BatchInsert, InsertType, SingleElementInsert}
 import com.bwsw.tstreams.agents.producer.{BasicProducer, BasicProducerOptions, ProducerCoordinationOptions}
 import com.bwsw.tstreams.converter.IConverter
 import com.bwsw.tstreams.coordination.transactions.transport.impl.TcpTransport
@@ -321,30 +321,71 @@ class UniversalFactory(envname: String = "T-streams") {
   // coordination scope
   propertyMap += (UF_Dictionary.Coordination.endpoints          -> "localhost:2181")
   propertyMap += (UF_Dictionary.Coordination.root               -> "/t-streams")
-  propertyMap += (UF_Dictionary.Coordination.ttl                -> 7)
-  propertyMap += (UF_Dictionary.Coordination.connection_timeout -> 7)
+
+  val Coordination_ttl_default                                  = 7
+  val Coordination_ttl_min                                      = 1
+  val Coordination_ttl_max                                      = 30
+  propertyMap += (UF_Dictionary.Coordination.ttl                -> Coordination_ttl_default)
+
+  val Coordination_connection_timeout_default                   = 7
+  val Coordination_connection_timeout_min                       = 1
+  val Coordination_connection_timeout_max                       = 30
+  propertyMap += (UF_Dictionary.Coordination.connection_timeout -> Coordination_connection_timeout_default)
 
   // stream scope
   propertyMap += (UF_Dictionary.Stream.name                 -> "test")
-  propertyMap += (UF_Dictionary.Stream.partitions           -> 1)
-  propertyMap += (UF_Dictionary.Stream.ttl                  -> 60 * 60 * 24)
+
+  val Stream_partitions_default                             = 1
+  val Stream_partitions_min                                 = 1
+  val Stream_partitions_max                                 = 100000000
+  propertyMap += (UF_Dictionary.Stream.partitions           -> Stream_partitions_default)
+
+  val Stream_ttl_default                                    = 60 * 60 * 24
+  val Stream_ttl_min                                        = 60
+  val Stream_ttl_max                                        = 315360000
+  propertyMap += (UF_Dictionary.Stream.ttl                  -> Stream_ttl_default)
   propertyMap += (UF_Dictionary.Stream.description          -> "Test stream")
 
   // producer scope
   propertyMap += (UF_Dictionary.Producer.name                                 -> "test-producer-1")
   propertyMap += (UF_Dictionary.Producer.master_bind_host                     -> "localhost")
   propertyMap += (UF_Dictionary.Producer.master_bind_port                     -> 18000)
-  propertyMap += (UF_Dictionary.Producer.master_timeout                       -> 5)
-  propertyMap += (UF_Dictionary.Producer.Transaction.ttl                      -> 6)
-  propertyMap += (UF_Dictionary.Producer.Transaction.open_maxwait             -> 5)
-  propertyMap += (UF_Dictionary.Producer.Transaction.keep_alive               -> 1)
-  propertyMap += (UF_Dictionary.Producer.Transaction.data_write_batch_size    -> 100)
+  val Producer_master_timeout_default                                         = 5
+  val Producer_master_timeout_min                                             = 1
+  val Producer_master_timeout_max                                             = 10
+  propertyMap += (UF_Dictionary.Producer.master_timeout                       -> Producer_master_timeout_default)
+
+  val Producer_transaction_ttl_default                                        = 6
+  val Producer_transaction_ttl_min                                            = 3
+  val Producer_transaction_ttl_max                                            = 15
+  propertyMap += (UF_Dictionary.Producer.Transaction.ttl                      -> Producer_transaction_ttl_default)
+
+  val Producer_transaction_open_maxwait_default                               = 5
+  val Producer_transaction_open_maxwait_min                                   = 1
+  val Producer_transaction_open_maxwait_max                                   = 10
+  propertyMap += (UF_Dictionary.Producer.Transaction.open_maxwait             -> Producer_transaction_open_maxwait_default)
+
+  val Producer_transaction_keep_alive_default                                 = 1
+  val Producer_transaction_keep_alive_min                                     = 1
+  val Producer_transaction_keep_alive_max                                     = 2
+  propertyMap += (UF_Dictionary.Producer.Transaction.keep_alive               -> Producer_transaction_keep_alive_default)
+
+  val Producer_transaction_data_write_batch_size_default                      = 100
+  val Producer_transaction_data_write_batch_size_min                          = 1
+  val Producer_transaction_data_write_batch_size_max                          = 1000
+  propertyMap += (UF_Dictionary.Producer.Transaction.data_write_batch_size    -> Producer_transaction_data_write_batch_size_default)
   propertyMap += (UF_Dictionary.Producer.Transaction.distribution_policy      -> UF_Dictionary.Producer.Transaction.Consts.DISTRIBUTION_POLICY_RR)
-  propertyMap += (UF_Dictionary.Producer.thread_pool                          -> 4)
+
+  val Producer_thread_pool_default                                            = 4
+  val Producer_thread_pool_min                                                = 1
+  val Producer_thread_pool_max                                                = 64
+  propertyMap += (UF_Dictionary.Producer.thread_pool                          -> Producer_thread_pool_default)
 
   // consumer scope
-  propertyMap += (UF_Dictionary.Consumer.transaction_preload              -> 10)
-  propertyMap += (UF_Dictionary.Consumer.data_preload                     -> 100)
+  val Consumer_transaction_preload_default                                = 10
+  propertyMap += (UF_Dictionary.Consumer.transaction_preload              -> Consumer_transaction_preload_default)
+  val Consumer_data_preload_default                                       = 100
+  propertyMap += (UF_Dictionary.Consumer.data_preload                     -> Consumer_data_preload_default)
   propertyMap += (UF_Dictionary.Consumer.Subscriber.bind_host             -> "localhost")
   propertyMap += (UF_Dictionary.Consumer.Subscriber.bind_port             -> 18001)
   propertyMap += (UF_Dictionary.Consumer.Subscriber.persistent_queue_path -> "/tmp")
@@ -491,16 +532,16 @@ class UniversalFactory(envname: String = "T-streams") {
       password        = password)
 
     assert(pAsString(UF_Dictionary.Stream.name) != null)
-    pAssertIntRange(pAsInt(UF_Dictionary.Stream.partitions, 1), 1, 100000000)
-    pAssertIntRange(pAsInt(UF_Dictionary.Stream.ttl, 60), 60, 315360000)
+    pAssertIntRange(pAsInt(UF_Dictionary.Stream.partitions, Stream_partitions_default), Stream_partitions_min, Stream_partitions_max)
+    pAssertIntRange(pAsInt(UF_Dictionary.Stream.ttl, Stream_ttl_default), Stream_ttl_min, Stream_ttl_max)
 
     // construct stream
     val stream = new BasicStream[Array[Byte]](
       name            = pAsString(UF_Dictionary.Stream.name),
-      partitions      = pAsInt(UF_Dictionary.Stream.partitions, -1),
+      partitions      = pAsInt(UF_Dictionary.Stream.partitions, Stream_partitions_default),
       metadataStorage = ms,
       dataStorage     = ds,
-      ttl             = pAsInt(UF_Dictionary.Stream.ttl, 60),
+      ttl             = pAsInt(UF_Dictionary.Stream.ttl, Stream_ttl_default),
       description     = pAsString(UF_Dictionary.Stream.description, ""))
 
     assert(pAsString(UF_Dictionary.Producer.master_bind_host) != null)
@@ -508,20 +549,21 @@ class UniversalFactory(envname: String = "T-streams") {
     assert(pAsString(UF_Dictionary.Coordination.endpoints)    != null)
     assert(pAsString(UF_Dictionary.Coordination.root)    != null)
 
-    pAssertIntRange(pAsInt(UF_Dictionary.Coordination.ttl, 30), 1, 30)
-    pAssertIntRange(pAsInt(UF_Dictionary.Producer.master_timeout, 5), 1, 10)
-    pAssertIntRange(pAsInt(UF_Dictionary.Coordination.connection_timeout, 5), 1, 30)
+    pAssertIntRange(pAsInt(UF_Dictionary.Coordination.ttl, Coordination_ttl_default), Coordination_ttl_min, Coordination_ttl_max)
+    pAssertIntRange(pAsInt(UF_Dictionary.Producer.master_timeout, Producer_master_timeout_default), Producer_master_timeout_min, Producer_master_timeout_max)
+    pAssertIntRange(pAsInt(UF_Dictionary.Coordination.connection_timeout, Coordination_connection_timeout_default),
+                        Coordination_connection_timeout_min, Coordination_connection_timeout_max)
 
     // construct coordination agent options
     val cao = new ProducerCoordinationOptions(
       agentAddress            = pAsString(UF_Dictionary.Producer.master_bind_host) + ":" + pAsString(UF_Dictionary.Producer.master_bind_port),
       zkHosts                 = getInetSocketAddressCompatibleHostList(pAsString(UF_Dictionary.Coordination.endpoints)),
       zkRootPath              = pAsString(UF_Dictionary.Coordination.root),
-      zkSessionTimeout        = pAsInt(UF_Dictionary.Coordination.ttl, 30),
+      zkSessionTimeout        = pAsInt(UF_Dictionary.Coordination.ttl, Coordination_ttl_default),
       isLowPriorityToBeMaster = isLowPriority,
       transport               = new TcpTransport,
-      transportTimeout        = pAsInt(UF_Dictionary.Producer.master_timeout, 5),
-      zkConnectionTimeout     = pAsInt(UF_Dictionary.Coordination.connection_timeout, 5))
+      transportTimeout        = pAsInt(UF_Dictionary.Producer.master_timeout, Producer_master_timeout_default),
+      zkConnectionTimeout     = pAsInt(UF_Dictionary.Coordination.connection_timeout, Coordination_connection_timeout_default))
 
 
     var writePolicy: AbstractPolicy = null
@@ -536,12 +578,23 @@ class UniversalFactory(envname: String = "T-streams") {
         "is supported currently in UniversalFactory.")
     }
 
-    pAssertIntRange(pAsInt(UF_Dictionary.Producer.Transaction.ttl, 6), 6, 30)
-    pAssertIntRange(pAsInt(UF_Dictionary.Producer.Transaction.keep_alive, 1), 1, 4)
+    pAssertIntRange(pAsInt(UF_Dictionary.Producer.Transaction.ttl, Producer_transaction_ttl_default), Producer_transaction_ttl_min, Producer_transaction_ttl_max)
+    pAssertIntRange(pAsInt(UF_Dictionary.Producer.Transaction.keep_alive, Producer_transaction_keep_alive_default), Producer_transaction_keep_alive_min, Producer_transaction_keep_alive_max)
+    assert(pAsInt(UF_Dictionary.Producer.Transaction.ttl, Producer_transaction_ttl_default) >=
+            pAsInt(UF_Dictionary.Producer.Transaction.keep_alive, Producer_transaction_keep_alive_default) * 3)
+
+    var insertType: InsertType = SingleElementInsert
+
+    val insertCnt = pAsInt(UF_Dictionary.Producer.Transaction.data_write_batch_size, Producer_transaction_data_write_batch_size_default)
+    pAssertIntRange(insertCnt,
+      Producer_transaction_data_write_batch_size_min, Producer_transaction_data_write_batch_size_max)
+
+    if (insertCnt > 1)
+        insertType = BatchInsert(insertCnt)
 
     val po = new BasicProducerOptions[USERTYPE, Array[Byte]](
-      transactionTTL                = pAsInt(UF_Dictionary.Producer.Transaction.ttl, 6),
-      transactionKeepAliveInterval  = pAsInt(UF_Dictionary.Producer.Transaction.keep_alive, 1),
+      transactionTTL                = pAsInt(UF_Dictionary.Producer.Transaction.ttl, Producer_transaction_ttl_default),
+      transactionKeepAliveInterval  = pAsInt(UF_Dictionary.Producer.Transaction.keep_alive, Producer_transaction_keep_alive_default),
       producerKeepAliveInterval     = 1, // TODO: deprecated, remove when https://github.com/bwsw/t-streams/issues/19 will be fixed!
       writePolicy                   = writePolicy,
       insertType                    = SingleElementInsert,
