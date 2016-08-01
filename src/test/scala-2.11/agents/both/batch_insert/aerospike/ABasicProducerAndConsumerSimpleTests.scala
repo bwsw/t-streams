2d8ac8e13f1286fa50c4bedcc1dc21416e556c35
Bitworks, Ltd <bitworks@bw-sw.com>
2016-08-01 16:08:37 +0700

    Prepared ABasicProducerAndConsumerSimpleTests

--- src/test/scala-2.11/agents/both/batch_insert/aerospike/ABasicProducerAndConsumerSimpleTests.scala
+++ src/test/scala-2.11/agents/both/batch_insert/aerospike/ABasicProducerAndConsumerSimpleTests.scala
@@ -8,6 +8,7 @@ import com.bwsw.tstreams.agents.producer.InsertionType.BatchInsert
 import com.bwsw.tstreams.agents.producer.{BasicProducer, BasicProducerOptions, ProducerCoordinationOptions, ProducerPolicies}
 import com.bwsw.tstreams.common.CassandraHelper
 import com.bwsw.tstreams.coordination.transactions.transport.impl.TcpTransport
+import com.bwsw.tstreams.env.TSF_Dictionary
 import com.bwsw.tstreams.streams.BasicStream
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
 import testutils._
@@ -18,52 +19,34 @@ import scala.util.control.Breaks._
 
 class ABasicProducerAndConsumerSimpleTests extends FlatSpec with Matchers with BeforeAndAfterAll with TestUtils {
 
-  val aerospikeInstForProducer = storageFactory.getInstance(aerospikeOptions)
-  val aerospikeInstForConsumer = storageFactory.getInstance(aerospikeOptions)
+  f.setProperty(TSF_Dictionary.Stream.name,"test_stream").
+    setProperty(TSF_Dictionary.Stream.partitions,3).
+    setProperty(TSF_Dictionary.Stream.ttl, 60 * 10).
+    setProperty(TSF_Dictionary.Coordination.connection_timeout, 7).
+    setProperty(TSF_Dictionary.Coordination.ttl, 7).
+    setProperty(TSF_Dictionary.Producer.master_timeout, 5).
+    setProperty(TSF_Dictionary.Producer.Transaction.ttl, 6).
+    setProperty(TSF_Dictionary.Producer.Transaction.keep_alive, 2).
+    setProperty(TSF_Dictionary.Consumer.transaction_preload, 10).
+    setProperty(TSF_Dictionary.Consumer.data_preload, 10)
 
-  //metadata storage instances
-  val metadataStorageInstForProducer = metadataStorageFactory.getInstance(
-    cassandraHosts = List(new InetSocketAddress("localhost", 9042)),
-    keyspace = randomKeyspace)
-  val metadataStorageInstForConsumer = metadataStorageFactory.getInstance(
-    cassandraHosts = List(new InetSocketAddress("localhost", 9042)),
-    keyspace = randomKeyspace)
+  val producer = f.getProducer[String](
+    name = "test_producer",
+    txnGenerator = LocalGeneratorCreator.getGen(),
+    converter = stringToArrayByteConverter,
+    partitions = List(0,1,2),
+    isLowPriority = false)
 
-  //stream instances for producer/consumer
-  val streamForProducer: BasicStream[Array[Byte]] = new BasicStream[Array[Byte]](
-    name = "test_stream",
-    partitions = 3,
-    metadataStorage = metadataStorageInstForProducer,
-    dataStorage = aerospikeInstForProducer,
-    ttl = 60 * 10,
-    description = "some_description")
+  val consumer = f.getConsumer[String](
+    name = "test_consumer",
+    txnGenerator = LocalGeneratorCreator.getGen(),
+    converter = arrayByteToStringConverter,
+    partitions = List(0,1,2),
+    offset = Oldest,
+    isUseLastOffset = true)
 
-  val streamForConsumer = new BasicStream[Array[Byte]](
-    name = "test_stream",
-    partitions = 3,
-    metadataStorage = metadataStorageInstForConsumer,
-    dataStorage = aerospikeInstForConsumer,
-    ttl = 60 * 10,
-    description = "some_description")
+  consumer.start()
 
-  val agentSettings = new ProducerCoordinationOptions(
-    agentAddress = "localhost:8888",
-    zkHosts = List(new InetSocketAddress("localhost", 2181)),
-    zkRootPath = "/unit",
-    zkSessionTimeout = 7000,
-    isLowPriorityToBeMaster = false,
-    transport = new TcpTransport,
-    transportTimeout = 5,
-    zkConnectionTimeout = 7)
-
-  //producer/consumer options
-  val producerOptions = new BasicProducerOptions[String](transactionTTL = 6, transactionKeepAliveInterval = 2, RoundRobinPolicyCreator.getRoundRobinPolicy(streamForProducer, List(0, 1, 2)), BatchInsert(batchSizeTestVal), LocalGeneratorCreator.getGen(), agentSettings, stringToArrayByteConverter)
-
-  val consumerOptions = new BasicConsumerOptions[String](transactionsPreload = 10, dataPreload = 7, arrayByteToStringConverter, RoundRobinPolicyCreator.getRoundRobinPolicy(streamForConsumer, List(0, 1, 2)), Oldest, LocalGeneratorCreator.getGen(), useLastOffset = true)
-
-  val producer = new BasicProducer("test_producer", streamForProducer, producerOptions)
-  var consumer = new BasicConsumer("test_consumer", streamForConsumer, consumerOptions)
-  consumer.start
 
   "producer, consumer" should "producer - generate one transaction, consumer - retrieve it with getAll method" in {
     CassandraHelper.clearMetadataTables(session, randomKeyspace)
