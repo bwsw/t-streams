eacdf3042e6f8ecd18a552aaeddcf36d613b50e8
Bitworks, Ltd <bitworks@bw-sw.com>
2016-08-02 17:55:34 +0700

    Added class for AsynchronousTransactionTests where to write asynchronous test for txns.

--- src/test/scala-2.11/agents/subscriber/AsynchronousTransactionTests.scala
+++ src/test/scala-2.11/agents/subscriber/AsynchronousTransactionTests.scala
@@ -6,6 +6,7 @@ import java.util.concurrent.locks.ReentrantLock
 
 import com.bwsw.tstreams.agents.consumer.Offsets.Oldest
 import com.bwsw.tstreams.agents.consumer.subscriber.{BasicSubscribingConsumer, BasicSubscriberCallback}
+import com.bwsw.tstreams.agents.producer.ProducerPolicies
 import com.bwsw.tstreams.debug.GlobalHooks
 import com.bwsw.tstreams.env.TSF_Dictionary
 import org.scalatest.{BeforeAndAfterAll, Matchers, FlatSpec}
@@ -17,7 +18,9 @@ import testutils.{LocalGeneratorCreator, TestUtils}
 class AsynchronousTransactionTests  extends FlatSpec with Matchers
   with BeforeAndAfterAll with TestUtils {
 
-  GlobalHooks.addHook("AfterCommitFailure", () => throw new RuntimeException)
+  // required for hooks to work
+  System.setProperty("DEBUG", "true")
+  System.setProperty(org.slf4j.impl.SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "DEBUG");
 
   f.setProperty(TSF_Dictionary.Stream.name,"test_stream").
     setProperty(TSF_Dictionary.Stream.partitions,3).
@@ -30,30 +33,89 @@ class AsynchronousTransactionTests  extends FlatSpec with Matchers
     setProperty(TSF_Dictionary.Consumer.transaction_preload, 10).
     setProperty(TSF_Dictionary.Consumer.data_preload, 10)
 
+
   val producer = f.getProducer[String](
     name = "test_producer",
     txnGenerator = LocalGeneratorCreator.getGen(),
     converter = stringToArrayByteConverter,
-    partitions = List(0,1,2),
+    partitions = List(0),
     isLowPriority = false)
 
-  val s1 = f.getSubscriber[String](
-    name = "test_subscriber",
-    txnGenerator = LocalGeneratorCreator.getGen(),
-    converter = arrayByteToStringConverter,
-    partitions = List(0,1,2),
-    offset = Oldest,
-    isUseLastOffset = true,
-    callback = new BasicSubscriberCallback[String] {
-      override def onEvent(
-                            subscriber: BasicSubscribingConsumer[String],
-                            partition: Int,
-                            transactionUuid: UUID): Unit = {
+  "Fire async checkpoint by producer and wait when complete" should "consumer get transaction from DB" in {
+    val l = new CountDownLatch(1)
+    GlobalHooks.addHook("AfterCommitFailure", () => {
+      l.countDown()
+    })
 
-      }})
+    val c = f.getConsumer[String](
+      name = "test_subscriber",
+      txnGenerator = LocalGeneratorCreator.getGen(),
+      converter = arrayByteToStringConverter,
+      partitions = List(0),
+      offset = Oldest,
+      isUseLastOffset = true)
 
-  "Fire async checkpoint by producer and wait when complete" should "consumer read checkpoint from DB" in {
+    val ptxn = producer.newTransaction(policy = ProducerPolicies.errorIfOpened)
+    ptxn.send("test")
+    ptxn.checkpoint(isSynchronous = false)
+    l.await()
+    c.start()
+    val ctxn = c.getTransaction
 
+    ctxn.isDefined shouldBe true
+    ptxn.getTxnUUID shouldBe ctxn.get.getTxnUUID
+    if(ctxn.isDefined)
+      c.checkpoint()
+  }
+
+  "Fire async checkpoint by producer (with exception) and wait when complete" should "consumer not get transaction from DB" in {
+    val l = new CountDownLatch(1)
+    GlobalHooks.addHook("PreCommitFailure", () => {
+      l.countDown()
+      throw new Exception("expected")
+    })
+
+    val c = f.getConsumer[String](
+      name = "test_subscriber",
+      txnGenerator = LocalGeneratorCreator.getGen(),
+      converter = arrayByteToStringConverter,
+      partitions = List(0),
+      offset = Oldest,
+      isUseLastOffset = true)
+
+    val ptxn = producer.newTransaction(policy = ProducerPolicies.errorIfOpened)
+    ptxn.send("test")
+    ptxn.checkpoint(isSynchronous = false)
+    l.await()
+    c.start()
+    val ctxn = c.getTransaction
+
+    ctxn.isDefined shouldBe false
+  }
+
+
+  "Fire async checkpoint by producer (with delay) and wait when complete" should "consumer not get transaction from DB" in {
+    val l = new CountDownLatch(1)
+    GlobalHooks.addHook("PreCommitFailure", () => {
+      l.await()
+      throw new Exception("expected")
+    })
+
+    val c = f.getConsumer[String](
+      name = "test_subscriber",
+      txnGenerator = LocalGeneratorCreator.getGen(),
+      converter = arrayByteToStringConverter,
+      partitions = List(0),
+      offset = Oldest,
+      isUseLastOffset = true)
+
+    val ptxn = producer.newTransaction(policy = ProducerPolicies.errorIfOpened)
+    ptxn.send("test")
+    ptxn.checkpoint(isSynchronous = false)
+    c.start()
+    val ctxn = c.getTransaction
+    l.countDown()
+    ctxn.isDefined shouldBe false
   }
 
 }
