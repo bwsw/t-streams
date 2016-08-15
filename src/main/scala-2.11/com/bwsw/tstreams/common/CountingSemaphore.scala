b0ad900499a4077c6801a69bd1f38708b598ef00
bwsw <ivan.a.kudryavtsev@gmail.com>
2016-08-15 09:28:09 +0700

    Fix to factory batch insert.

---
+++ src/main/scala-2.11/com/bwsw/tstreams/common/CountingSemaphore.scala
@@ -0,0 +1,24 @@
+package com.bwsw.tstreams.common
+
+/**
+  * Created by ivan on 15.08.16.
+  */
+class CountingSemaphore(initial: Int = 0) {
+  private var signals: Int = initial
+  def take(): Int = this.synchronized {
+    while(this.signals == 0) wait()
+    signals -= 1
+    signals
+  }
+
+  def release(): Int = this.synchronized {
+    signals +=1
+    notify()
+    signals
+  }
+
+  def waitZero() = this.synchronized {
+    while(this.signals != 0) wait()
+  }
+}
+
