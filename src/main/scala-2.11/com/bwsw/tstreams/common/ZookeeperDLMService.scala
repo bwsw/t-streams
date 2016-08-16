85b574d09848d8724ec7afa9eb4887f0b8ce25d9
Bitworks, Ltd <bitworks@bw-sw.com>
2016-08-16 13:18:22 +0700

    Fix #203. Review of ZookeeperDLMService is done.

--- src/main/scala-2.11/com/bwsw/tstreams/common/ZookeeperDLMService.scala
+++ src/main/scala-2.11/com/bwsw/tstreams/common/ZookeeperDLMService.scala
@@ -12,6 +12,14 @@ import org.slf4j.LoggerFactory
 
 import scala.collection.JavaConverters._
 
+/**
+  * shared objects for ZookeeperDLMService
+  */
+object ZookeeperDLMService {
+  val logger = LoggerFactory.getLogger(this.getClass)
+  val serializer = new JsonSerializer
+}
+
 //TODO test it harder on zk ephemeral nodes elimination
 /**
   * @param prefix           Prefix path for all created entities
@@ -19,89 +27,105 @@ import scala.collection.JavaConverters._
   * @param zkSessionTimeout Zk session timeout to connect
   */
 class ZookeeperDLMService(prefix: String, zkHosts: List[InetSocketAddress], zkSessionTimeout: Int, connectionTimeout: Long) {
-  private val logger = LoggerFactory.getLogger(this.getClass)
   private val st = Amount.of(new Integer(zkSessionTimeout), com.twitter.common.quantity.Time.SECONDS)
   private val ct = Amount.of(connectionTimeout, com.twitter.common.quantity.Time.SECONDS)
-  private val hosts = zkHosts.asJava
-  private val twitterZkClient = new ZooKeeperClient(st, hosts)
-  private val zkClient = twitterZkClient.get(ct)
-  private val serializer = new JsonSerializer
-  private val map = scala.collection.mutable.Map[String, DistributedLockImpl]()
 
-  logger.debug("Zookeeper session timeout is set to " + st)
-  logger.debug("Zookeeper connection timeout is set to " + ct)
+  private val lockMap = scala.collection.mutable.Map[String, DistributedLockImpl]()
 
-  def getLock(path: String): DistributedLockImpl = {
-    if (map.contains(prefix + path))
-      map(prefix + path)
+  private val twitterZkClient = new ZooKeeperClient(st, zkHosts.asJava)
+  private val zkClient        = twitterZkClient.get(ct)
+
+  if(ZookeeperDLMService.logger.isDebugEnabled) {
+    ZookeeperDLMService.logger.debug("Zookeeper session timeout is set to " + st)
+    ZookeeperDLMService.logger.debug("Zookeeper connection timeout is set to " + ct)
+  }
+
+  /**
+    * receives DLM lock object which can be used later
+    *
+    * @param path
+    * @return
+    */
+  def getLock(path: String): DistributedLockImpl = this.synchronized {
+    if (lockMap.contains(prefix + path))
+      lockMap(prefix + path)
     else {
       val lock = new DistributedLockImpl(twitterZkClient, prefix + path)
-      map += (prefix + path -> lock)
+      lockMap += (prefix + path -> lock)
       lock
     }
   }
 
-  def create[T](path: String, data: T, createMode: CreateMode) = {
-    val serialized = serializer.serialize(data)
+  /**
+    * Allows to create path in Zookeeper in proper manner (with locks)
+    *
+    * @param path
+    * @param data
+    * @param createMode
+    * @tparam T
+    * @return
+    */
+  def create[T](path: String, data: T, createMode: CreateMode) = this.synchronized {
+    val serialized = ZookeeperDLMService.serializer.serialize(data)
     var initPath = prefix + path.reverse.dropWhile(_ != '/').reverse.dropRight(1)
+
     if (initPath.isEmpty)
       initPath = "/"
-    if (zkClient.exists(initPath, null) == null) {
-      LockUtil.withZkLockOrDieDo[Unit](getLock(s"/locks/create_path_lock"), (100, TimeUnit.SECONDS), Some(logger), () => {
-        if (zkClient.exists(initPath, null) == null)
-          createPathRecursive(initPath, CreateMode.PERSISTENT) })
-    }
-    if (zkClient.exists(prefix + path, null) == null)
-      zkClient.create(prefix + path, serialized.getBytes, Ids.OPEN_ACL_UNSAFE, createMode)
-    else {
-      throw new IllegalStateException("path already exist")
-    }
+
+    LockUtil.withZkLockOrDieDo[Unit](getLock(s"/locks/create_path_lock"), (100, TimeUnit.SECONDS), Some(ZookeeperDLMService.logger), () => {
+      if (zkClient.exists(initPath, null) == null)
+        createPathRecursive(initPath, CreateMode.PERSISTENT) })
+
+    LockUtil.withZkLockOrDieDo[Unit](getLock(s"/locks/create_path_lock"), (100, TimeUnit.SECONDS), Some(ZookeeperDLMService.logger), () => {
+      if (zkClient.exists(prefix + path, null) == null)
+          zkClient.create(prefix + path, serialized.getBytes, Ids.OPEN_ACL_UNSAFE, createMode)
+      else {
+        throw new IllegalStateException(s"Path ${prefix + path} requested for creation already exist.")
+      }})
   }
 
-  def setWatcher(path: String, watcher: Watcher): Unit = {
-    if (zkClient.exists(prefix + path, null) == null) {
-      LockUtil.withZkLockOrDieDo[Unit](getLock(s"/locks/watcher_path_lock"), (100, TimeUnit.SECONDS), Some(logger), () => {
-        if (zkClient.exists(prefix + path, null) == null)
-          createPathRecursive(prefix + path, CreateMode.PERSISTENT) })
-    }
+  def setWatcher(path: String, watcher: Watcher): Unit = this.synchronized {
+    LockUtil.withZkLockOrDieDo[Unit](getLock(s"/locks/watcher_path_lock"), (100, TimeUnit.SECONDS), Some(ZookeeperDLMService.logger), () => {
+      if (zkClient.exists(prefix + path, null) == null)
+        createPathRecursive(prefix + path, CreateMode.PERSISTENT) })
     zkClient.getData(prefix + path, watcher, null)
   }
 
-  def notify(path: String): Unit = {
+  def notify(path: String): Unit = this.synchronized {
     if (zkClient.exists(prefix + path, null) != null) {
       zkClient.setData(prefix + path, null, -1)
     }
   }
 
-  def setData(path: String, data: Any): Unit = {
-    val string = serializer.serialize(data)
+  def setData(path: String, data: Any): Unit = this.synchronized {
+    val string = ZookeeperDLMService.serializer.serialize(data)
     zkClient.setData(prefix + path, string.getBytes, -1)
   }
 
-  def exist(path: String): Boolean = {
+  def exist(path: String): Boolean = this.synchronized {
     zkClient.exists(prefix + path, null) != null
   }
 
-  def get[T: Manifest](path: String): Option[T] = {
+  def get[T: Manifest](path: String): Option[T] = this.synchronized {
     if (zkClient.exists(prefix + path, null) == null)
       None
     else {
       val data = zkClient.getData(prefix + path, null, null)
-      Some(serializer.deserialize[T](new String(data)))
+      Some(ZookeeperDLMService.serializer.deserialize[T](new String(data)))
     }
   }
 
-  def getAllSubNodesData[T: Manifest](path: String): Option[List[T]] = {
+  def getAllSubNodesData[T: Manifest](path: String): Option[List[T]] = this.synchronized {
     if (zkClient.exists(prefix + path, null) == null)
       None
     else {
       val subNodes = zkClient.getChildren(prefix + path, null).asScala.map(x => zkClient.getData(prefix + path + "/" + x, null, null))
-      val data = subNodes.flatMap(x => List(serializer.deserialize[T](new String(x)))).toList
+      val data = subNodes.flatMap(x => List(ZookeeperDLMService.serializer.deserialize[T](new String(x)))).toList
       Some(data)
     }
   }
 
-  def getAllSubPath(path: String): Option[List[String]] = {
+  def getAllSubPath(path: String): Option[List[String]] = this.synchronized {
     if (zkClient.exists(prefix + path, null) == null)
       None
     else {
@@ -110,11 +134,11 @@ class ZookeeperDLMService(prefix: String, zkHosts: List[InetSocketAddress], zkSe
     }
   }
 
-  def delete(path: String) = {
+  def delete(path: String) = this.synchronized {
     zkClient.delete(prefix + path, -1)
   }
 
-  def deleteRecursive(path: String): Unit = {
+  def deleteRecursive(path: String): Unit = this.synchronized {
     val children = zkClient.getChildren(prefix + path, null, null).asScala
     if (children.nonEmpty) {
       children.foreach { x => deleteRecursive(path + "/" + x) }
@@ -123,7 +147,7 @@ class ZookeeperDLMService(prefix: String, zkHosts: List[InetSocketAddress], zkSe
     zkClient.delete(prefix + path, -1)
   }
 
-  private def createPathRecursive(path: String, mode: CreateMode) = {
+  private def createPathRecursive(path: String, mode: CreateMode) = this.synchronized {
     val splits = path.split("/").filter(x => x != "")
     def createRecursive(path: List[String], acc: List[String]): Unit = path match {
       case Nil =>
@@ -140,13 +164,15 @@ class ZookeeperDLMService(prefix: String, zkHosts: List[InetSocketAddress], zkSe
     createRecursive(splits.toList.drop(1), List(splits.head))
   }
 
-  def isZkConnected =
+  def isZkConnected = this.synchronized {
     zkClient.getState == States.CONNECTED
+  }
 
-  def getSessionTimeout =
+  def getSessionTimeout = {
     zkClient.getSessionTimeout
+  }
 
-  def close() = {
+  def close() = this.synchronized {
     twitterZkClient.close()
   }
 }
