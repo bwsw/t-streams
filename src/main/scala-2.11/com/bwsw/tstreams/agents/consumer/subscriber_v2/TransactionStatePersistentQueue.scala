e25fbcef3454bf89337a5bd7328d9029e610fa8f
Bitworks, Ltd <bitworks@bw-sw.com>
2016-08-19 14:06:42 +0700

    Added serialization/deserialization with Jackson.

--- src/main/scala-2.11/com/bwsw/tstreams/agents/consumer/subscriber_v2/TransactionStatePersistentQueue.scala
+++ src/main/scala-2.11/com/bwsw/tstreams/agents/consumer/subscriber_v2/TransactionStatePersistentQueue.scala
@@ -1,19 +1,28 @@
 package com.bwsw.tstreams.agents.consumer.subscriber_v2
 
+import java.io.StringWriter
+
 import com.bwsw.tstreams.common.AbstractPersistentQueue
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.scala.DefaultScalaModule
 
 /**
-  * Created by ivan on 19.08.16.
+  * Created by Ivan Kudryavtsev on 19.08.16.
   */
 class TransactionStatePersistentQueue(basePath: String)
   extends AbstractPersistentQueue[List[TransactionState]](basePath: String) {
 
+  val mapper = new ObjectMapper()
+  mapper.registerModule(DefaultScalaModule)
+
   override def serialize(elt: Object): String = {
-    null
+    val out = new StringWriter
+    mapper.writeValue(out, elt.asInstanceOf[List[TransactionState]])
+    out.toString()
   }
 
   override def deserialize(data: String): Object = {
-    Nil
+    mapper.readValue(data, classOf[List[TransactionState]]).asInstanceOf[Object]
   }
 
 }
