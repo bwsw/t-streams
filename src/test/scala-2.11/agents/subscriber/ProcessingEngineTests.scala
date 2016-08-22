d5ecd452db49ab8864aa43d649e01bef1f168376
Bitworks, Ltd <bitworks@bw-sw.com>
2016-08-22 12:10:21 +0700

    Started ProcessingEngine tests. Implemented enqueueLastTransactionFromDB.

--- src/test/scala-2.11/agents/subscriber/ProcessingEngineTests.scala
+++ src/test/scala-2.11/agents/subscriber/ProcessingEngineTests.scala
@@ -1,10 +1,12 @@
 package agents.subscriber
 
 import java.util.UUID
+import java.util.concurrent.TimeUnit
 
-import com.bwsw.tstreams.agents.consumer.subscriber_v2.{Callback, QueueBuilder, ProcessingEngine}
+import com.bwsw.tstreams.agents.consumer.subscriber_v2.{TransactionState, Callback, QueueBuilder, ProcessingEngine}
 import com.bwsw.tstreams.agents.consumer.{Transaction, TransactionOperator}
 import com.bwsw.tstreams.common.FirstFailLockableTaskExecutor
+import com.bwsw.tstreams.coordination.messages.state.TransactionStatus
 import com.datastax.driver.core.utils.UUIDs
 import org.scalatest.{Matchers, FlatSpec}
 
@@ -16,7 +18,9 @@ class ProcessingEngineOperatorTestImpl extends TransactionOperator[String] {
   for(i <- 0 until TOTAL)
     txns += new Transaction[String](0, UUIDs.timeBased(), 1, -1)
 
-  override def getLastTransaction(partition: Int): Option[Transaction[String]] = None
+  var lstTransaction: Option[Transaction[String]] = None
+
+  override def getLastTransaction(partition: Int): Option[Transaction[String]] = lstTransaction
 
   override def getTransactionById(partition: Int, uuid: UUID): Option[Transaction[String]] = None
 
@@ -38,13 +42,45 @@ class ProcessingEngineOperatorTestImpl extends TransactionOperator[String] {
   * Created by Ivan Kudryavtsev on 22.08.16.
   */
 class ProcessingEngineTests extends FlatSpec with Matchers {
-  it should "create Processing engine" in {
-    val q = new QueueBuilder.InMemory().generateQueueObject(0)
-    val pe = new ProcessingEngine[String](new ProcessingEngineOperatorTestImpl(), Set[Int](0), q, new Callback[String] {
-      override def onEvent(consumer: TransactionOperator[String], partition: Int, uuid: UUID, count: Int): Unit = {
 
-      }
-    }, new FirstFailLockableTaskExecutor("lf"))
+  val cb = new Callback[String] {
+    override def onEvent(consumer: TransactionOperator[String], partition: Int, uuid: UUID, count: Int): Unit = {}
   }
+
+  val e = new FirstFailLockableTaskExecutor("lf")
+
+  "constructor" should "create Processing engine" in {
+    val q = new QueueBuilder.InMemory().generateQueueObject(0)
+    val pe = new ProcessingEngine[String](new ProcessingEngineOperatorTestImpl(), Set[Int](0), q, cb, e)
+  }
+
+  "enqueueLastTransactionFromDB" should "not Enqueue last transaction state to Queue if it's not defined" in {
+    val q = new QueueBuilder.InMemory().generateQueueObject(0)
+    val pe = new ProcessingEngine[String](new ProcessingEngineOperatorTestImpl(), Set[Int](0), q, cb, e)
+    pe.enqueueLastTransactionFromDB(0)
+    val elt = q.get(200, TimeUnit.MILLISECONDS)
+    elt shouldBe null
+  }
+
+  "enqueueLastTransactionFromDB" should "enqueue last transaction state to Queue if it's newer than we have in our database" in {
+    val q = new QueueBuilder.InMemory().generateQueueObject(0)
+    val c = new ProcessingEngineOperatorTestImpl()
+    val pe = new ProcessingEngine[String](c, Set[Int](0), q, cb, e)
+    c.lstTransaction = Option[Transaction[String]](new Transaction(0, UUIDs.timeBased(), 1, -1))
+    pe.enqueueLastTransactionFromDB(0)
+    val elt = q.get(200, TimeUnit.MILLISECONDS)
+    elt.head.uuid shouldBe c.lstTransaction.get.getTxnUUID()
+  }
+
+  "enqueueLastTransactionFromDB" should "not enqueue last transaction state to Queue if it's older than we have in our database" in {
+    val q = new QueueBuilder.InMemory().generateQueueObject(0)
+    val c = new ProcessingEngineOperatorTestImpl()
+    c.lstTransaction = Option[Transaction[String]](new Transaction(0, UUIDs.timeBased(), 1, -1))
+    val pe = new ProcessingEngine[String](c, Set[Int](0), q, cb, e)
+    pe.enqueueLastTransactionFromDB(0)
+    val elt = q.get(200, TimeUnit.MILLISECONDS)
+    elt shouldBe null
+  }
+
 
 }
