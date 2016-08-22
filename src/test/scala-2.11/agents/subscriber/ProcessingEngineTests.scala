cf984b402fb11132d6734230ea41de838cab9002
Bitworks, Ltd <bitworks@bw-sw.com>
2016-08-22 10:50:41 +0700

    Started ProcessingEngine tests.

--- src/test/scala-2.11/agents/subscriber/ProcessingEngineTests.scala
+++ src/test/scala-2.11/agents/subscriber/ProcessingEngineTests.scala
@@ -2,7 +2,9 @@ package agents.subscriber
 
 import java.util.UUID
 
+import com.bwsw.tstreams.agents.consumer.subscriber_v2.{Callback, QueueBuilder, ProcessingEngine}
 import com.bwsw.tstreams.agents.consumer.{Transaction, TransactionOperator}
+import com.bwsw.tstreams.common.FirstFailLockableTaskExecutor
 import com.datastax.driver.core.utils.UUIDs
 import org.scalatest.{Matchers, FlatSpec}
 
@@ -29,12 +31,20 @@ class ProcessingEngineOperatorTestImpl extends TransactionOperator[String] {
 
   override def getPartitions(): Set[Int] = Set[Int](0)
 
-  override def getCurrentOffset(partition: Int): UUID = ???
+  override def getCurrentOffset(partition: Int): UUID = UUIDs.timeBased()
 }
 
 /**
   * Created by Ivan Kudryavtsev on 22.08.16.
   */
 class ProcessingEngineTests extends FlatSpec with Matchers {
+  it should "create Processing engine" in {
+    val q = new QueueBuilder.InMemory().generateQueueObject(0)
+    val pe = new ProcessingEngine[String](new ProcessingEngineOperatorTestImpl(), Set[Int](0), q, new Callback[String] {
+      override def onEvent(consumer: TransactionOperator[String], partition: Int, uuid: UUID, count: Int): Unit = {
+
+      }
+    }, new FirstFailLockableTaskExecutor("lf"))
+  }
 
 }
