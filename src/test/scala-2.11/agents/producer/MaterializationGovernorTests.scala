f3e310971b1f1f7a8ecca1d6153eebef7bd39e5d
Bitworks, Ltd <bitworks@bw-sw.com>
2016-08-29 09:32:34 +0700

    Added materialization governor tests.

--- src/test/scala-2.11/agents/producer/MaterializationGovernorTests.scala
+++ src/test/scala-2.11/agents/producer/MaterializationGovernorTests.scala
@@ -1,8 +1,39 @@
 package agents.producer
 
+import java.util.concurrent.CountDownLatch
+
+import com.bwsw.tstreams.agents.producer.MaterializationGovernor
+import org.scalatest.{Matchers, FlatSpec}
+
 /**
   * Created by ivan on 28.08.16.
   */
-class MaterializationGovernorTests {
+class MaterializationGovernorTests  extends FlatSpec with Matchers  {
+  it should "init, protect, unprotect, await unprotection in single thread" in {
+    val mg = new MaterializationGovernor(Set(0))
+    mg.protect(0)
+    mg.unprotect(0)
+    mg.awaitUnprotected(0)
+  }
+
+  it should "init, protect, unprotect, await unprotection in two threads" in {
+    val mg = new MaterializationGovernor(Set(0))
+    mg.protect(0)
+    var end: Long = 0
+    val l = new CountDownLatch(1)
+    val t = new Thread(new Runnable {
+      override def run(): Unit = {
+        mg.awaitUnprotected(0)
+        end = System.currentTimeMillis()
+        l.countDown()
+      }
+    })
+    t.start()
+    val start = System.currentTimeMillis()
+    Thread.sleep(10)
+    mg.unprotect(0)
+    l.await()
+    end - start >= 10 shouldBe true
+  }
 
 }
