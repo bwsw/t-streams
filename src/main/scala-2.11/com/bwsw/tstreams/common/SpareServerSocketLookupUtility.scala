766b6bdea97ffa9b099aa5c4d952d3056bc429ec
Bitworks, Ltd <bitworks@bw-sw.com>
2016-09-06 09:48:21 +0700

    Implemented Port finder.

--- src/main/scala-2.11/com/bwsw/tstreams/common/SpareServerSocketLookupUtility.scala
+++ src/main/scala-2.11/com/bwsw/tstreams/common/SpareServerSocketLookupUtility.scala
@@ -1,11 +1,28 @@
 package com.bwsw.tstreams.common
 
+import java.io.IOException
+import java.net.{InetAddress, ServerSocket}
+
 /**
-  * Created by ivan on 05.09.16.
+  * Created by Ivan Kudryavtsev on 05.09.16.
   */
 object SpareServerSocketLookupUtility {
 
-  private def checkIfAvailable(hostOrIp: String, port: Int): Boolean = ???
+  private def checkIfAvailable(hostOrIp: String, port: Int): Boolean = {
+    var ss: ServerSocket = null
+
+    try {
+      ss = new ServerSocket(port, 1, InetAddress.getByName(hostOrIp))
+      ss.setReuseAddress(true)
+      ss.close()
+      return true
+    } catch {
+      case e: IOException =>
+    } finally {
+      if (ss != null) ss.close()
+    }
+    return false
+  }
 
   def findSparePort(hostOrIp: String, fromPort: Int, toPort: Int): Option[Int] = synchronized {
     (fromPort to toPort).find(port => checkIfAvailable(hostOrIp, port))
