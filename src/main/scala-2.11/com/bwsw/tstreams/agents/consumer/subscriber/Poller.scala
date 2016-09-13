ac8368d5e28f2ea57b08ea81f7a41693dd6ad0e8
bwsw <ivan.a.kudryavtsev@gmail.com>
2016-09-13 23:29:32 +0700

    Improved executor service.

--- src/main/scala-2.11/com/bwsw/tstreams/agents/consumer/subscriber/Poller.scala
+++ src/main/scala-2.11/com/bwsw/tstreams/agents/consumer/subscriber/Poller.scala
@@ -6,6 +6,11 @@ package com.bwsw.tstreams.agents.consumer.subscriber
 class Poller[T](processingEngine: ProcessingEngine[T], interval: Int) extends Runnable {
   override def run(): Unit = {
     processingEngine.handleQueue(interval)
-    processingEngine.getExecutor().submit(new Poller[T](processingEngine, interval))
+    try {
+      processingEngine.getExecutor().submit(new Poller[T](processingEngine, interval))
+    } catch {
+      case e: IllegalStateException =>
+        Subscriber.logger.warn(e.getMessage)
+    }
   }
 }
