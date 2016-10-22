f9c85e4aec4e3ff0ec9de86cfe5caa569934f858
bwsw <ivan.a.kudryavtsev@gmail.com>
2016-10-22 12:31:41 +0700

    Started Thrift transaction server works.

--- src/main/thrift/transactionServer.thrift
+++ src/main/thrift/transactionServer.thrift
@@ -6,10 +6,44 @@ enum TransactionStates {
     Invalid      = 3
 }
 
+struct Transaction {
+    1: required i64 transactionID
+    2: required TransactionStates state
+    3: required i32 quantity
+    4: required i32 timestamp
+}
+
+struct Stream {
+    1: required i32 partitions
+    2: optional string description
+}
+
 service TransactionServerService {
     // auth api
     string authenticate(1: string login, 2: string password),
 
     // transaction api
-    bool putTransaction(1: string token, 2: string stream, 3: i32 partition, 4: i64 interval, 5: i64 transaction, 6: TransactionStates state, 7: i32 quantity, 8: i32 timestamp),
+    bool putTransaction(1: string token, 2: string stream, 3: i32 partition, 4: i64 interval,
+                        5: i64 transaction, 6: TransactionStates state, 7: i32 quantity, 8: i32 timestamp),
+
+    list<Transaction> scanTransctions(1: string token, 2: string stream, 3: i32 partition, 4: i64 interval),
+
+    i32 scanTransctionsCRC32(1: string token, 2: string stream, 3: i32 partition, 4: i64 interval),
+
+    bool delTransaction(1: string token, 2: string stream, 3: i32 partition, 4: i64 interval, 5: i64 transaction),
+
+    bool putStream(1: string token, 2: string stream, 3: i32 partition, 4: i32 partitions, 5: string description),
+
+    bool delStream(1: string token, 2: string stream),
+
+    Stream getStream(1: string token, 2: string stream),
+
+    bool setConsumerState(1: string token, 2: string name, 3: string stream, 4: i32 partition, 5: i64 transaction),
+
+    i64 getConsumerState(1: string token, 2: string name, 3: string stream, 4: i32 partition),
+
+    bool putTransactionData(1: string token, 2: string stream, 3: i32 partition, 4: i64 transaction, 5: i32 from, 6: i32 to, 7: list<binary> data),
+
+    list <binary> getTransactionData(1: string token, 2: string stream, 3: i32 partition, 4: i64 transaction, 5: i32 from, 6: i32 to),
+
 }
