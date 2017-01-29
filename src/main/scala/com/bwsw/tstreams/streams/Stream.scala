4dd12a99b94cf1ddc89c59937fe28a3872c06913
bwsw <ivan.a.kudryavtsev@gmail.com>
2017-01-29 12:16:08 +0700

    Implemented stream service.

--- src/main/scala/com/bwsw/tstreams/streams/Stream.scala
+++ src/main/scala/com/bwsw/tstreams/streams/Stream.scala
@@ -2,6 +2,9 @@ package com.bwsw.tstreams.streams
 
 import com.bwsw.tstreams.common.StorageClient
 
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
 /**
   * Settings of stream in metadata storage
   *
@@ -14,13 +17,19 @@ case class StreamSettings(name: String, partitions: Int, ttl: Int, description: 
 
 object Stream {
 
+  var OP_TIMEOUT = 1.minute
+
   /**
     * Retrieving stream with concrete name
     *
     * @param name Stream name to fetch from database
     * @return StreamSettings
     */
-  def getStream(storageClient: StorageClient, name: String): Option[StreamSettings] = ???
+  def getStream(storageClient: StorageClient, name: String): Option[StreamSettings] = {
+    val stream = Await.result(storageClient.client.getStream(name), OP_TIMEOUT)
+    Some(StreamSettings(name = stream.name, partitions = stream.partitions, ttl = stream.ttl, description = stream.description.fold("")(x => x)))
+  }
+
   /**
     * Create stream with parameters
     *
@@ -30,11 +39,9 @@ object Stream {
     * @param description Stream arbitrary description and com.bwsw.tstreams.metadata, etc.
     * @return StreamSettings
     */
-  def createStream(storageClient: StorageClient,
-                   name: String,
-                   partitions: Int,
-                   ttl: Int,
-                   description: String): Unit = ???
+  def createStream(storageClient: StorageClient, name: String, partitions: Int, ttl: Int, description: String): Unit = {
+    Await.result(storageClient.client.putStream(name, partitions, Some(description), ttl), OP_TIMEOUT)
+  }
 
   /**
     * Alternate stream with parameters
@@ -45,14 +52,18 @@ object Stream {
     * @param description Stream arbitrary description and com.bwsw.tstreams.metadata, etc.
     * @return StreamSettings
     */
-  def changeStream(storageClient: StorageClient, name: String, partitions: Int, ttl: Int, description: String): Unit = ???
+  def changeStream(storageClient: StorageClient, name: String, partitions: Int, ttl: Int, description: String): Unit = {
+    Await.result(storageClient.client.putStream(name, partitions, Some(description), ttl), OP_TIMEOUT)
+  }
 
   /**
     * Deleting concrete stream
     *
     * @param name Stream name to delete
     */
-  def deleteStream(storageClient: StorageClient, name: String): Unit = ???
+  def deleteStream(storageClient: StorageClient, name: String): Unit = {
+    Await.result(storageClient.client.delStream(name), OP_TIMEOUT)
+  }
 
   /**
     * Checking that concrete stream exist
@@ -60,7 +71,10 @@ object Stream {
     * @param name Stream name to check if exists
     * @return Exist stream or not
     */
-  def isExist(storageClient: StorageClient, name: String): Boolean = ???
+  def isExist(storageClient: StorageClient, name: String): Boolean = {
+    Await.result(storageClient.client.doesStreamExist(name), OP_TIMEOUT)
+  }
+
 }
 
 /**
