46e212c7a7198ce3f53f082bfd77cd60bc9ea54d
Bitworks, Ltd <bitworks@bw-sw.com>
2017-02-19 15:08:11 +0700

    TStreamsFactoryTest checked. Passes.

--- src/test/scala/env/TStreamsFactoryTest.scala
+++ src/test/scala/env/TStreamsFactoryTest.scala
@@ -3,6 +3,8 @@ package env
 import com.bwsw.tstreams.agents.consumer.Offset.Oldest
 import com.bwsw.tstreams.agents.consumer.{ConsumerTransaction, TransactionOperator}
 import com.bwsw.tstreams.env.ConfigurationOptions
+import com.bwsw.tstreams.streams.StreamService
+import testutils.TestStorageServer
 
 /**
   * Created by Ivan Kudryavtsev on 23.07.16.
@@ -14,6 +16,8 @@ import testutils.TestUtils
 class TStreamsFactoryTest extends FlatSpec with Matchers with BeforeAndAfterAll with TestUtils {
   f.setProperty(ConfigurationOptions.Stream.ttlSec, 60 * 10)
 
+  val srv = TestStorageServer.get()
+  val storageClient = f.getStorageClient()
 
   "If copied" should "contain same data" in {
     val n1 = f.copy()
@@ -57,19 +61,25 @@ class TStreamsFactoryTest extends FlatSpec with Matchers with BeforeAndAfterAll 
   }
 
   "UniversalFactory.getSubscriber" should "return subscriber object" in {
+
     val sub = f.getSubscriber(
       name = "test-subscriber",
       partitions = Set(0),
       offset = Oldest,
       callback = (consumer: TransactionOperator, transaction: ConsumerTransaction) => {})
 
+
     sub != null shouldEqual true
+
+    StreamService.createStream(storageClient, sub.stream.name, 1, 24 * 3600, "sample-desc")
+
     sub.start()
     Thread.sleep(1000) // TODO: fix it. Bug #31
     sub.stop()
   }
 
   override def afterAll(): Unit = {
+    TestStorageServer.dispose(srv)
     super.afterAll()
   }
 
