b5553dbe2001114f8a5f7bb4bc95ac0df643d2e7
bwsw <ivan.a.kudryavtsev@gmail.com>
2017-04-02 22:50:10 +0700

    fixed IntersectingTransactionsTests

--- src/test/scala/agents/integration/v20/IntersectingTransactionsTests.scala
+++ src/test/scala/agents/integration/v20/IntersectingTransactionsTests.scala
@@ -1,4 +1,4 @@
-package agents.integration
+package agents.integration.v20
 
 import java.util.concurrent.CountDownLatch
 
@@ -7,7 +7,7 @@ import com.bwsw.tstreams.agents.consumer.{ConsumerTransaction, TransactionOperat
 import com.bwsw.tstreams.agents.producer.NewTransactionProducerPolicy
 import com.bwsw.tstreams.env.ConfigurationOptions
 import org.scalatest.{BeforeAndAfterAll, FlatSpec, Matchers}
-import testutils.TestUtils
+import testutils.{TestStorageServer, TestUtils}
 
 import scala.collection.mutable.ListBuffer
 
@@ -19,15 +19,18 @@ class IntersectingTransactionsTests extends FlatSpec with Matchers with BeforeAn
   f.setProperty(ConfigurationOptions.Stream.name, "test_stream").
     setProperty(ConfigurationOptions.Stream.partitionsCount, 3).
     setProperty(ConfigurationOptions.Stream.ttlSec, 60 * 10).
-    setProperty(ConfigurationOptions.Coordination.connectionTimeoutMs, 7).
-    setProperty(ConfigurationOptions.Coordination.sessionTimeoutMs, 7).
-    setProperty(ConfigurationOptions.Producer.transportTimeoutMs, 5).
-    setProperty(ConfigurationOptions.Producer.Transaction.ttlMs, 3).
-    setProperty(ConfigurationOptions.Producer.Transaction.keepAliveMs, 1).
-    setProperty(ConfigurationOptions.Consumer.transactionPreload, 10).
+    setProperty(ConfigurationOptions.Coordination.connectionTimeoutMs, 7000).
+    setProperty(ConfigurationOptions.Coordination.sessionTimeoutMs, 7000).
+    setProperty(ConfigurationOptions.Producer.transportTimeoutMs, 5000).
+    setProperty(ConfigurationOptions.Producer.Transaction.ttlMs, 6000).
+    setProperty(ConfigurationOptions.Producer.Transaction.keepAliveMs, 2000).
+    setProperty(ConfigurationOptions.Consumer.transactionPreload, 500).
     setProperty(ConfigurationOptions.Consumer.dataPreload, 10)
 
 
+  val srv = TestStorageServer.get()
+  val storageClient = f.getStorageClient()
+  storageClient.createStream("test_stream", 3, 24 * 3600, "")
 
   it should "handle all transactions produced by two different producers, the first ends first started " in {
     val bp = ListBuffer[Long]()
@@ -92,6 +95,7 @@ class IntersectingTransactionsTests extends FlatSpec with Matchers with BeforeAn
   }
 
   override def afterAll(): Unit = {
+    TestStorageServer.dispose(srv)
     onAfterAll()
   }
 
