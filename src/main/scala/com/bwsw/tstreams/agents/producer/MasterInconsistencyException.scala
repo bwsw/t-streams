42be39e0d5c24774359f7d9a32b24d15a6fd26c7
Bitworks, Ltd <bitworks@bw-sw.com>
2017-05-08 14:59:58 +0700

    Implemented feature #585 - code - test

---
+++ src/main/scala/com/bwsw/tstreams/agents/producer/MasterInconsistencyException.scala
@@ -0,0 +1,7 @@
+package com.bwsw.tstreams.agents.producer
+
+/**
+  * Created by ivan on 08.05.17.
+  */
+
+class MasterInconsistencyException(message: String) extends IllegalArgumentException(message)
